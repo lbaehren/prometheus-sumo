03958a36eb7052864152075454073e8b05435a61
Lars Baehren <lbaehren@gmail.com>
2012-04-11 20:37:19 +0200

    Make package installation dependent on availability of package manager.

--- environment/InstallPackagesRedhat.cmake
+++ environment/InstallPackagesRedhat.cmake
@@ -8,39 +8,43 @@ find_program (YUM_EXECUTABLE yum
   )
 
 ##_______________________________________________________________________________
-## 
+## Installation of the individual packages
 
-foreach (package_redhat
-    ruby
-    rubygems
-    rdoc
-    ri
-    irb
-    eruby
-    libxml2-devel
-    mysql-server
-    mysql-client
-    mysql-devel
-    apache2
-    #  (mod_ruby???)
-    postfix
-    subversion
-    subversion-tools
-    libapache2-svn
-    ImageMagick
-    ImageMagick-devel 
-    )
-
-  ## Installation instructions for the package
-  ExternalProject_Add (install_package_${package_redhat}
-    PREFIX ${CMAKE_CURRENT_BINARY_DIR}
-    CONFIGURE_COMMAND ""
-    BUILD_COMMAND ""
-    INSTALL_COMMAND ${YUM_EXECUTABLE} install ${package_redhat}
-    COMMENT "Installing Redhat package ${package_redhat} ..."
-    )
-
-  ## Connection with collective target
-  add_dependencies (InstallPackages install_package_${package_redhat})
+if (YUM_EXECUTABLE)
   
-endforeach (package_redhat)
+  foreach (package_redhat
+      ruby
+      rubygems
+      rdoc
+      ri
+      irb
+      eruby
+      libxml2-devel
+      mysql-server
+      mysql-client
+      mysql-devel
+      apache2
+      #  (mod_ruby???)
+      postfix
+      subversion
+      subversion-tools
+      libapache2-svn
+      ImageMagick
+      ImageMagick-devel 
+      )
+    
+    ## Installation instructions for the package
+    ExternalProject_Add (install_package_${package_redhat}
+      PREFIX ${CMAKE_CURRENT_BINARY_DIR}
+      CONFIGURE_COMMAND ""
+      BUILD_COMMAND ""
+      INSTALL_COMMAND ${YUM_EXECUTABLE} install ${package_redhat}
+      COMMENT "Installing Redhat package ${package_redhat} ..."
+      )
+    
+    ## Connection with collective target
+    add_dependencies (InstallPackages install_package_${package_redhat})
+    
+  endforeach (package_redhat)
+  
+endif (YUM_EXECUTABLE)
