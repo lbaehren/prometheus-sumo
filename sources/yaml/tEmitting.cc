e94c4c103245cf0327f0e954123e0cda0ffe82cc
Lars Baehren <lbaehren@gmail.com>
2012-05-04 18:14:09 +0200

    Separate output from the individual test routines.

--- sources/yaml/tEmitting.cc
+++ sources/yaml/tEmitting.cc
@@ -12,10 +12,12 @@
 
 void hello_world ()
 {
+  std::cout << "\n[tEmitting::hello_world]\n" << std::endl;
+
   YAML::Emitter out;
   out << "Hello, World!";
   
-  std::cout << "Here's the output YAML:\n" << out.c_str(); // prints "Hello, World!"
+  std::cout << "Here's the output YAML:\n" << out.c_str() << std::endl;
 }
 
 //_______________________________________________________________________________
@@ -23,12 +25,19 @@ void hello_world ()
 
 void emit_list ()
 {
+  std::cout << "\n[tEmitting::emit_list]\n" << std::endl;
+
   YAML::Emitter out;
+  
+  /* Assemble the sequence */
   out << YAML::BeginSeq;
   out << "eggs";
   out << "bread";
   out << "milk";
   out << YAML::EndSeq;
+
+  /* Write out the sequence */
+  std::cout << out.c_str() << std::endl;
 }
 
 //_______________________________________________________________________________
@@ -36,13 +45,20 @@ void emit_list ()
 
 void emit_map ()
 {
+  std::cout << "\n[tEmitting::emit_map]\n" << std::endl;
+
   YAML::Emitter out;
+
+  /* Assemble the map */
   out << YAML::BeginMap;
   out << YAML::Key << "name";
   out << YAML::Value << "Barack Obama";
   out << YAML::Key << "children";
   out << YAML::Value << YAML::BeginSeq << "Sasha" << "Malia" << YAML::EndSeq;
   out << YAML::EndMap;
+
+  /* Write the map to standard output */
+  std::cout << out.c_str() << std::endl;
 }
 
 // ==============================================================================
