0b064a44f5feb78d793f4e19f4f083e892bacf8d
Lars Baehren <lbaehren@gmail.com>
2012-05-24 18:02:31 +0200

    Check first of port is installed already.

--- packages/PackagesOSX.cmake
+++ packages/PackagesOSX.cmake
@@ -21,17 +21,33 @@ if (PORT_EXECUTABLE)
       rb-rubygems
       ruby
       )
-    
-    ## Installation instructions for the package ___________
-    
-    add_custom_command (
-      TARGET InstallPackages
-      POST_BUILD
-      COMMAND ${PORT_EXECUTABLE} install ${varPackage}
+
+    ## Check if package if installed already _______________
+
+    execute_process (
+      COMMAND ${PORT_EXECUTABLE} list ${varPackage}
       WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
-      COMMENT "Installing MacPorts package ${varPackage} ..."
+      RESULT_VARIABLE PORT_RESULT_VARIABLE
+      OUTPUT_VARIABLE PORT_OUTPUT_VARIABLE
+      ERROR_VARIABLE PORT_ERROR_VARIABLE
+      OUTPUT_STRIP_TRAILING_WHITESPACE
       )
 
+    ## Installation instructions for the package ___________
+    
+    if (PORT_OUTPUT_VARIABLE)
+      message (STATUS "Checking whether port ${varPackage} is installed - yes")
+    else (PORT_OUTPUT_VARIABLE)
+      message (STATUS "Checking whether port ${varPackage} is installed - no")
+      add_custom_command (
+	TARGET InstallPackages
+	POST_BUILD
+	COMMAND ${PORT_EXECUTABLE} install ${varPackage}
+	WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+	COMMENT "Installing MacPorts package ${varPackage} ..."
+	)
+    endif (PORT_OUTPUT_VARIABLE)
+    
   endforeach (varPackage)
   
 endif (PORT_EXECUTABLE)
