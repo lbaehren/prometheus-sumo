2dd20ea1d3f9921757a09d1f57ed70ae71c0fc0b
Lars Baehren <lbaehren@gmail.com>
2012-07-11 16:55:30 +0200

    Set address information.

--- sources/lib/Institution.cc
+++ sources/lib/Institution.cc
@@ -3,6 +3,17 @@
 
 namespace prometheus {
   
+  // ============================================================================
+  //
+  //  Construction
+  //
+  // ============================================================================
+
+  /*!
+    \param name        -- The name of the institution.
+    \param title       -- The title of the institution.
+    \param description -- A description for the institution.
+  */
   Institution::Institution (std::string const &name,
 			    std::string const &title,
 			    std::string const &description)
@@ -12,5 +23,27 @@ namespace prometheus {
     itsDescription = description;
   }
   
-  
+  // ============================================================================
+  //
+  //  Public methods
+  //
+  // ============================================================================
+
+  /*!
+    \param addressline -- The address line for the institution.
+    \param postalcode  -- The postal code for the institution.
+    \param city        -- 
+    \param country     -- 
+  */
+  void Institution::setAddress (std::string const &addressline,
+				std::string const &postalcode,
+				std::string const &city,
+				std::string const &country)
+  {
+    itsAddressline = addressline;
+    itsPostalcode  = postalcode;
+    itsCity        = city;
+    itsCountry     = country;
+  }
+    
 }
--- sources/lib/Institution.h
+++ sources/lib/Institution.h
@@ -18,9 +18,13 @@ namespace prometheus {  //  namespace prometheus -- BEGIN
 
     // === Private data =========================================================
 
+    //! The name of the institution
     std::string itsName;
+    //! The title of the institution
     std::string itsTitle;
+    //! A description for the institution
     std::string itsDescription;
+    //! The address line of the institution
     std::string itsAddressline;
     std::string itsPostalcode;
     std::string itsCity;
@@ -29,12 +33,11 @@ namespace prometheus {  //  namespace prometheus -- BEGIN
 
     // === Private methods ======================================================
 
-    // === Public methods =======================================================
-
   public:
 
     // === Construction =========================================================
 
+    //! Argumented constructor
     Institution (std::string const &name,
 		 std::string const &title="",
 		 std::string const &description="");
@@ -63,20 +66,36 @@ namespace prometheus {  //  namespace prometheus -- BEGIN
     inline std::string description () {
       return itsDescription;
     }
+    //! Set a description for the institution
+    inline void setDescription (std::string const &description) {
+      itsDescription = description;
+    }
 
-    //! Get the address line of the institution
+    //! Get the address line for the institution
     inline std::string addressline () {
       return itsAddressline;
     }
+    //! Set the address line for the institution
+    inline void setAddressline (std::string const &addressline) {
+      itsAddressline = addressline;
+    }
 
-    //! Get the postal code of the institution
+    //! Get the postal code for the institution
     inline std::string postalcode () {
       return itsPostalcode;
     }
-
+    //! Set the postal code for the institution
+    inline void setPostalcode (std::string const &postalcode) {
+      itsPostalcode = postalcode;
+    }
+    
     //! Get the city of the institution
     inline std::string city () {
       return itsCity;
+    }
+    //! Set the city of the institution
+    inline void setCity (std::string const &city) {
+      itsCity = city;
     }
 
     //! Get the country of the institution
@@ -88,6 +107,14 @@ namespace prometheus {  //  namespace prometheus -- BEGIN
     inline std::string email () {
       return itsEmail;
     }
+
+    // === Public methods =======================================================
+
+    //! Set the address information for the institution
+    void setAddress (std::string const &addressline,
+		     std::string const &postalcode,
+		     std::string const &city,
+		     std::string const &country);
 
     // === Static methods =======================================================
 
