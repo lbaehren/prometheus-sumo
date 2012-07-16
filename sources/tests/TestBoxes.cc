90316c28ee0937297365320187de8807f8adc4c9
Lars Baehren <lbaehren@gmail.com>
2012-07-16 17:33:47 +0200

    Simple test for models.

---
+++ sources/tests/TestBoxes.cc
@@ -0,0 +1,20 @@
+
+#include <iostream>
+#include <string>
+
+#include <Models/Account.h>
+#include <Models/CollectionBox.h>
+
+// ==============================================================================
+//
+//  Main function
+//
+// ==============================================================================
+
+int main ()
+{
+  /* Create an account */
+  prometheus::Account account (0, "sid@iceage.org");
+
+  return  0;
+}
