3736d7b95117d427386ed90271c2348f79e0a786
Lars Baehren <lbaehren@gmail.com>
2012-09-03 18:52:59 +0200

    Connect sources and extended documentation.

---
+++ documentation/Support/XML.md
@@ -0,0 +1,17 @@
+
+XML    {#xml}
+===
+
+\tableofcontents
+
+\tableofcontents
+
+\section xml_overview Overview
+
+\test TestXML.cc
+
+\section xml_references References
+
+  - [XML Path Language](http://www.w3.org/TR/xpath) (W3C Recommendation)
+  - [XPath Tutorial](http://www.w3schools.com/xpath/default.asp)
+  - [Free C or C++ XML Parser Libraries](http://lars.ruoff.free.fr/xmlcpp)
--- sources/tests/TestXML.cc
+++ sources/tests/TestXML.cc
@@ -4,11 +4,22 @@
 
 #include <tinyxml2.h>
 
+//______________________________________________________________________________
+//                                                              test_char_buffer
+
+/*!
+  \brief Test parsing a simple string stored in character buffer
+  \param teststring -- Simple string containing some \ref xml syntax.
+  \return status -- Return status of the function; returns non-zero in case an
+                    an error is caught.
+*/
 int test_char_buffer (std::string const &teststring="<element/>")
 {
   std::cout << "\n[TestXML::test_char_buffer]\n" << std::endl;
 
   int status = 0;
+  
+  std::cout << "--> Input string = " << teststring << std::endl;
   
   tinyxml2::XMLDocument doc;
   doc.Parse(teststring.c_str());
@@ -16,6 +27,8 @@ int test_char_buffer (std::string const &teststring="<element/>")
    
    return status;
 }
+
+// === Main function ===========================================================
 
 /*!
   \file TestXML.cc
