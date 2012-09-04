a9237a819b283bdae45faafc583bc072238a9d59
Lars Baehren <lbaehren@gmail.com>
2012-09-04 18:07:36 +0200

    Data structures for further dumps.

--- sources/tests/TestXMLBoost.cc
+++ sources/tests/TestXMLBoost.cc
@@ -22,21 +22,6 @@
   \file TestXMLBoost.cc
   \ingroup prometheus
   \author Lars Baehren
-
-  \code{.xml}
-<row>
-  <Höhe>97</Höhe>
-  <Material>Öl auf Leinwand</Material>
-  <KünstlerIn>Hegewald, Michael</KünstlerIn>
-  <Gattung>Malerei</Gattung>
-  <Ob_f41>284</Ob_f41>
-  <Datierung>1989</Datierung>
-  <Einheit>cm</Einheit>
-  <Breite>138</Breite>
-  <Titel>Berlin</Titel>
-  <Standort>Kunstarchiv Beeskow</Standort>
-</row>
-  \endcode
  */
 
 #include <iostream>
@@ -53,7 +38,25 @@ using boost::property_tree::ptree;
 
 // === Data structures =========================================================
 
-struct Row {
+/*!
+  \brief Item in the collection of the Beeskow Kunstarchiv
+  
+    \code{.xml}
+<row>
+  <Höhe>97</Höhe>
+  <Material>Öl auf Leinwand</Material>
+  <KünstlerIn>Hegewald, Michael</KünstlerIn>
+  <Gattung>Malerei</Gattung>
+  <Ob_f41>284</Ob_f41>
+  <Datierung>1989</Datierung>
+  <Einheit>cm</Einheit>
+  <Breite>138</Breite>
+  <Titel>Berlin</Titel>
+  <Standort>Kunstarchiv Beeskow</Standort>
+</row>
+  \endcode
+*/
+struct ItemBeeskow {
   //! Height of the item, "Höhe"
   std::string height;
   //! Material used in the item, "Material"
@@ -76,7 +79,98 @@ struct Row {
   std::string location;
 };
 
+/*!
+  \brief Item in the collection theoleik.xml
+  
+  \code
+  <ROW MODID="0" RECORDID="12640214">
+    <bildreferenz>1000005.jpg</bildreferenz>
+    <abbildungsnachweis>Marguerat, D., Das enfant terrible des Christentums, in: Paulus. Ein unbequemer Apostel. Welt und Umwelt der Bibel 20 (Stuttgart 2001) 4</abbildungsnachweis>
+    <copyright></copyright>
+    <freigabe>Prometheus</freigabe>
+    <standort>Vatikan, Vatikanische Museen</standort>
+    <titel>Gemme: Paulus. Ausschnitt: Paulus</titel>
+    <herkunft>Rom (I), Grabstätte des Asellus</herkunft>
+    <datierung>um 313</datierung>
+    <kuenstlerin></kuenstlerin>
+    <material></material>
+    <gattung></gattung>
+    <schlagwort></schlagwort>
+    <masse></masse>
+    <zusatz></zusatz>
+  </ROW>
+  \endcode
+*/
+struct ItemTheolEik {
+  //! bildreferenz
+  std::string image;
+  //! abbildungsnachweis
+  std::string reference;
+  //! copyright
+  std::string copyright;
+  //! freigabe
+  std::string license;
+  //! standort
+  std::string location;
+  //! titel
+  std::string title;
+  //! herkunft
+  std::string origin;
+  //! datierung
+  std::string date;
+  //! kuenstlerin
+  std::string artist;
+  //! material
+  std::string material;
+  //! gattung
+  std::string category;
+  //! schlagwort
+  std::string keyword;
+  //! masse
+  std::string mass;
+  //! zusatz
+  std::string annex;
+};
+
+/*!
+  \brief Item in the collection kassel.xml
+
+  \code{.xml}
+<row>
+  <bild_nr>G8001</bild_nr>
+  <datierung>1783-1786</datierung>
+  <gattung>Gartenarchitektur</gattung>
+  <inventar_nr>GS 6258</inventar_nr>
+  <kuenstler>Jussow, Heinrich Christoph (Zeichner)</kuenstler>
+  <objekt>Tempel</objekt>
+  <objekt_id>11846</objekt_id>
+  <titel>Entwurf zu einem Gartentempel, Aufriß</titel>
+</row>
+  \endcode
+*/
+struct ItemKassel {
+  //! bild_nr
+  std::string image;
+  //! datierung
+  std::string date;
+  //! gattung
+  std::string category;
+  //! inventar_nr
+  std::string inventary;
+  //! kuenstler
+  std::string artist;
+  //! objekt
+  std::string object;
+  //! objekt_id
+  std::string objectID;
+  //! titel
+  std::string title;
+};
+
 // === Functions ===============================================================
+
+//______________________________________________________________________________
+//                                                      read_beeskow_kunstarchiv
 
 /*!
   \brief Read database for Beeskow Kunstarchiv
@@ -85,7 +179,7 @@ struct Row {
   \retval collection -- Array with the items in the colleciton
  */
 int read_beeskow_kunstarchiv (std::istream & infile,
-			      std::vector<Row> &collection)
+                              std::vector<ItemBeeskow> &collection)
 {
   ptree pt;
 
@@ -95,7 +189,7 @@ int read_beeskow_kunstarchiv (std::istream & infile,
   std::cout << "-- Parsin XML document ..." << std::endl;
   BOOST_FOREACH( ptree::value_type const& v, pt.get_child("root") ) {
     if( v.first == "row" ) {
-      Row r;
+      ItemBeeskow r;
       // r.artist   = v.second.get<std::string>("KünstlerIn");
       r.location = v.second.get<std::string>("Standort");
       r.title    = v.second.get<std::string>("Titel");
@@ -112,6 +206,39 @@ int read_beeskow_kunstarchiv (std::istream & infile,
   return 0;
 }
 
+//______________________________________________________________________________
+//                                                                 read_theoleik
+
+/*!
+  \brief Read database for Institut für Evangelische Theologie
+
+  \param infile -- Input stream connected to the XML dump for the collection.
+  \retval collection -- Array with the items in the colleciton
+ */
+int read_theoleik (std::istream & infile,
+                   std::vector<ItemTheolEik> &collection)
+{
+  ptree pt;
+
+  std::cout << "-- Reading XML input file ..." << std::endl;
+  read_xml(infile, pt);
+
+  std::cout << "-- Parsin XML document ..." << std::endl;
+  BOOST_FOREACH( ptree::value_type const& v, pt.get_child("dataroot") ) {
+    if( v.first == "row" ) {
+      ItemTheolEik node;
+      node.location = v.second.get<std::string>("Standort");
+      node.title    = v.second.get<std::string>("Titel");
+      node.date     = v.second.get<std::string>("Datierung");
+      node.category = v.second.get<std::string>("Gattung");
+      collection.push_back(node);
+    }
+  }
+
+  return 0;
+  return 0;
+}
+                              
 // === Main function ===========================================================
 
 /*!
@@ -122,7 +249,7 @@ int main(int argc, char* argv[])
   int status = 0;
   std::string filename;
 
-  /*  */
+  /* Parse command line parameters */
   if ( argc>1 ) {
     /* Get the name of the testdata ... */
     filename = std::string(argv[1]);
@@ -131,7 +258,7 @@ int main(int argc, char* argv[])
     
     if (infile.is_open()) {
 
-      std::vector<Row> collection;
+      std::vector<ItemBeeskow> collection;
 
       /* Parse the contents of the document */
       read_beeskow_kunstarchiv (infile, collection);
