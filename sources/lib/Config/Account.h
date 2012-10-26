da467789e584d2e5a6d06884c04b62c3ba5a3db2
Lars Baehren <lbaehren@gmail.com>
2012-10-26 14:46:41 +0200

    Class providing adapter to account configuration settings.

---
+++ sources/lib/Config/Account.h
@@ -0,0 +1,92 @@
+/***************************************************************************
+ *   Copyright (C) 2012                                                    *
+ *   Lars B"ahren (lbaehren@gmail.com)                                     *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#ifndef CONFIG_ACCOUNT_H
+#define CONFIG_ACCOUNT_H
+
+#include "ConfigFileBase.h"
+
+namespace prometheus {   // namespace prometheus -- BEGIN
+
+  namespace config {   // namespace config -- BEGIN
+
+    /*!
+      \file Account.h
+      \class Account
+      \ingroup prometheus
+      \ingroup config
+      \brief Configuration settings for an account
+      \test testConfigAccount.cc
+      \author Lars Baehren
+      \date 2012-10-25
+
+      This class provides an adapter to read configuration data for an account
+      inside the \ref pandora web application.
+      \include account.yml
+
+    */
+    class Account : public ConfigFileBase {
+
+      //! Parameters available for list display, search and user
+      std::map<std::string, std::vector<std::string> > itsColumns;
+
+      //! Store the data from an individual node
+      bool storeNode (YAML::Iterator const &it);
+
+    public:
+
+      // === Construction =======================================================
+
+      //! Default constructor
+      Account ();
+
+      //! Argumented constructor
+      Account (std::string const &filename);
+
+      // === Parameter access ===================================================
+
+      //! Get the number of parameter columns
+      inline size_t nofColumns () {
+        return itsColumns.size();
+      }
+
+      //! Get the names of the paramer columns
+      std::set<std::string> columnNames ();
+
+      //! Get the column names for a given \c name
+      std::vector<std::string> columnNamesFor (std::string const &name);
+
+      // === Public functions ===================================================
+
+      //! Provide a summary of the object's internal parameters and status
+      inline void summary () {
+        summary (std::cout);
+      }
+
+      //! Provide a summary of the object's internal parameters and status
+      void summary (std::ostream &os);
+
+    };
+
+  }   // namespace config -- BEGIN
+
+}   // namespace prometheus -- END
+
+#endif
