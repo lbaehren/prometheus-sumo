989a2b117aec2f06a84190dd3aa7124547db9914
Lars Baehren <lbaehren@gmail.com>
2012-11-02 09:47:01 +0100

    Read list of required Ruby gems from configuration file, instead of heaving the information embedded within the configuration script.

--- packages/RubyGems.cmake
+++ packages/RubyGems.cmake
@@ -14,93 +14,75 @@ set (UNICODE_REQUIRED_VERSION "0.1.1"    )
 
 if (GEM_EXECUTABLE)
 
-  message (STATUS "Checking Ruby Gems")
+  if (config_gems_yml)
 
-  ## Initialize counter
-  set (NOF_GEMS       0 )
-  set (NOF_GEMS_FOUND 0 )
+    message (STATUS "Checking Ruby Gems")
 
-  foreach (varPackage
-      apache_image_resizer
-      apache_secure_download
-      ar_mailer
-      blackwinter-gnuplot
-      blackwinter-ipaddress
-      capistrano
-      exifr
-      fastercsv
-      gruff
-      highline
-      hpricot
-      ipaddress
-      jekyll
-      jekyll-localization
-      jekyll-pagination
-      jekyll-tagging
-      json
-      libxml-ext
-      libxml-ruby
-      lockfile
-      mail
-      mime-types
-      mongrel
-      mongrel_cluster
-      mysql
-      oauth
-      pbkdf2
-      pdf-reader
-      piston
-      rake
-      redcarpet
-      ruby-backports
-      ruby-debug
-      ruby-filemagic
-      ruby-hmac
-      ruby-nuggets
-      unicode
-      wadl
-      yard
+    ## Read configuration file with the list of required gems
+    file (STRINGS ${config_gems_yml} _configGems
+      REGEX "name:"
       )
 
-    ## increment counter
-    math (EXPR NOF_GEMS "${NOF_GEMS}+1")
+    ## Initialize counters
+    set (NOF_GEMS       0 )
+    set (NOF_GEMS_FOUND 0 )
 
-    ##______________________________________________________
-    ## In order to unnecessary installation processes, we
-    ## first check if the Gem is installed already; if this
-    ## this is the case, we skip over the package, otherwise
-    ## it is added to the list available through the
-    ## 'InstallGems' target.
+    foreach (varPackage ${_configGems})
 
-    execute_process (
-      COMMAND ${GEM_EXECUTABLE} list ${varPackage}
-      WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
-      RESULT_VARIABLE GEM_RESULT_VARIABLE
-      OUTPUT_VARIABLE GEM_OUTPUT_VARIABLE
-      ERROR_VARIABLE GEM_ERROR_VARIABLE
-      OUTPUT_STRIP_TRAILING_WHITESPACE
-      )
+      ## Extract the name of the gems from the line in the configuration file
+      string (REGEX REPLACE "- name: " "" varPackage ${varPackage})
 
-    if (GEM_OUTPUT_VARIABLE)
-      if (CONFIGURE_VERBOSE)
-        message ("   [OK] ${varPackage}")
-      endif (CONFIGURE_VERBOSE)
-      math (EXPR NOF_GEMS_FOUND "${NOF_GEMS_FOUND}+1")
-    else (GEM_OUTPUT_VARIABLE)
-      ## Feedback
-      message ("   [--] ${varPackage}")
-      ## Installation instructions
-      add_custom_command (
-        TARGET InstallGems
-        POST_BUILD
-        COMMAND export GEM_HOME=${CMAKE_INSTALL_PREFIX}/lib/ruby/gems/${RUBY_VERSION_SERIES}
-        COMMAND ${GEM_EXECUTABLE} install ${varPackage}
+      ## increment counter
+      math (EXPR NOF_GEMS "${NOF_GEMS}+1")
+
+      ##______________________________________________________
+      ## In order to unnecessary installation processes, we
+      ## first check if the Gem is installed already; if this
+      ## this is the case, we skip over the package, otherwise
+      ## it is added to the list available through the
+      ## 'InstallGems' target.
+
+      execute_process (
+        COMMAND ${GEM_EXECUTABLE} list ${varPackage}
         WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
-        COMMENT "Installing Ruby Gem ${varPackage} ..."
+        RESULT_VARIABLE GEM_RESULT_VARIABLE
+        OUTPUT_VARIABLE GEM_OUTPUT_VARIABLE
+        ERROR_VARIABLE GEM_ERROR_VARIABLE
+        OUTPUT_STRIP_TRAILING_WHITESPACE
       )
-    endif (GEM_OUTPUT_VARIABLE)
 
-  endforeach (varPackage)
+      if (GEM_OUTPUT_VARIABLE)
+
+        ## Feedback
+        if (CONFIGURE_VERBOSE)
+          message ("   [OK] ${varPackage}")
+        endif (CONFIGURE_VERBOSE)
+        ## Counter update
+        math (EXPR NOF_GEMS_FOUND "${NOF_GEMS_FOUND}+1")
+
+      else (GEM_OUTPUT_VARIABLE)
+
+        ## Feedback
+        message ("   [--] ${varPackage}")
+        ## Installation instructions
+        add_custom_command (
+          TARGET InstallGems
+          POST_BUILD
+          COMMAND export GEM_HOME=${CMAKE_INSTALL_PREFIX}/lib/ruby/gems/${RUBY_VERSION_SERIES}
+          COMMAND ${GEM_EXECUTABLE} install ${varPackage}
+          WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+          COMMENT "Installing Ruby Gem ${varPackage} ..."
+        )
+
+      endif (GEM_OUTPUT_VARIABLE)
+
+    endforeach (varPackage)
+
+  else (config_gems_yml)
+
+    message (STATUS "Missing gems.yml configuration file!")
+
+  endif (config_gems_yml)
 
   ##________________________________________________________
   ## Gems for which a specific version is required
