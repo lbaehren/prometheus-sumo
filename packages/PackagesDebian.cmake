914eb5bcc2aee24d46ace57005401ab6ac508216
Lars Baehren <lbaehren@gmail.com>
2012-11-02 12:10:49 +0100

    Move package list out of the script processing them.

--- packages/PackagesDebian.cmake
+++ packages/PackagesDebian.cmake
@@ -1,5 +1,4 @@
 
-
 ##_______________________________________________________________________________
 ## Check for required tools
 
@@ -18,73 +17,66 @@ find_program (DPKG_EXECUTABLE dpkg
 
 if (APT_EXECUTABLE AND DPKG_EXECUTABLE)
 
-  message (STATUS "Checking Debian packages")
+  if (config_packages_debian_yml)
 
-  ## Initialize counters
-  set (NOF_PACKAGES       0 )
-  set (NOF_PACKAGES_FOUND 0 )
+    message (STATUS "Checking Debian packages")
 
-  foreach (varPackage
-      ## Some basic packages which should be installed to begin with
-      build-essential
-      cmake
-      git-svn
-      subversion
-      doxygen
-      libboost-dev
-      texlive-full
-      ## Packages required by prometheus/pandora
-      ruby${REQUIRED_VERSION_RUBY}
-      ruby${REQUIRED_VERSION_RUBY}-dev
-      rubygems
-      ri
-      rake
-      libmysql-ruby${REQUIRED_VERSION_RUBY}
-      libtermios-ruby${REQUIRED_VERSION_RUBY}
-      librmagick-ruby${REQUIRED_VERSION_RUBY}
-      libruby${REQUIRED_VERSION_RUBY}
-      mysql-server
-      mysql-client
-      libmysqlclient-dev
-      apache2
-      libapache2-mod-ruby
-      libapache2-mod-proxy-html
-      libxml2-dev
-      libmagic-dev
-      libmagickwand-dev
+    ## Initialize counters
+    set (NOF_PACKAGES       0 )
+    set (NOF_PACKAGES_FOUND 0 )
+
+    ## Read configuration file with the list of required gems
+    file (STRINGS ${config_packages_debian_yml} _configPackages
+      REGEX "name:"
       )
 
-    ## Inrement package counter
-    math (EXPR NOF_PACKAGES "${NOF_PACKAGES}+1")
+    foreach (varPackage ${_configPackages})
 
-    ## Installation instructions for the package; check
-    ## first, if the package is installed already __________
+      ## Inrement package counter
+      math (EXPR NOF_PACKAGES "${NOF_PACKAGES}+1")
 
-    execute_process (
-      COMMAND ${DPKG_EXECUTABLE} --get-selections ${varPackage}
-      WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
-      RESULT_VARIABLE DPKG_RESULT_VARIABLE
-      OUTPUT_VARIABLE DPKG_OUTPUT_VARIABLE
-      ERROR_VARIABLE DPKG_ERROR_VARIABLE
+      ## Extract the name of the gems from the line in the configuration file
+      string (REGEX REPLACE "- name: " "" varPackage ${varPackage})
+
+      ## Installation instructions for the package; check
+      ## first, if the package is installed already __________
+
+      execute_process (
+        COMMAND ${DPKG_EXECUTABLE} --get-selections ${varPackage}
+        WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+        RESULT_VARIABLE DPKG_RESULT_VARIABLE
+        OUTPUT_VARIABLE DPKG_OUTPUT_VARIABLE
+        ERROR_VARIABLE DPKG_ERROR_VARIABLE
       )
 
-    if (DPKG_OUTPUT_VARIABLE)
-      if (CONFIGURE_VERBOSE)
-	message ("   [OK] ${varPackage}")
-      endif (CONFIGURE_VERBOSE)
-      math (EXPR NOF_PACKAGES_FOUND "${NOF_PACKAGES_FOUND}+1")
-    else (DPKG_OUTPUT_VARIABLE)
-      message ("   [--] ${varPackage}")
-      add_custom_command (
-	TARGET InstallPackages
-	POST_BUILD
-	COMMAND ${APT_EXECUTABLE} install ${varPackage}
-	WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
-	COMMENT "Installing Debian package ${varPackage} ..."
-	)
-    endif (DPKG_OUTPUT_VARIABLE)
+      if (DPKG_OUTPUT_VARIABLE)
 
-  endforeach (varPackage)
+        ## Feedback
+        if (CONFIGURE_VERBOSE)
+          message ("   [OK] ${varPackage}")
+        endif (CONFIGURE_VERBOSE)
+        ## Increment counter
+        math (EXPR NOF_PACKAGES_FOUND "${NOF_PACKAGES_FOUND}+1")
+
+      else (DPKG_OUTPUT_VARIABLE)
+        message ("   [--] ${varPackage}")
+        add_custom_command (
+          TARGET InstallPackages
+          POST_BUILD
+          COMMAND ${APT_EXECUTABLE} install ${varPackage}
+          WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+          COMMENT "Installing Debian package ${varPackage} ..."
+        )
+
+      endif (DPKG_OUTPUT_VARIABLE)
+
+    endforeach (varPackage)
+
+  else (config_packages_debian_yml)
+
+    message (STATUS "Missing packages_debian.yml configuration file!")
+
+  endif (config_packages_debian_yml)
 
   ## Summary of package check
 
@@ -101,4 +93,3 @@ if (APT_EXECUTABLE AND DPKG_EXECUTABLE)
     )
 
 endif (APT_EXECUTABLE AND DPKG_EXECUTABLE)
-
