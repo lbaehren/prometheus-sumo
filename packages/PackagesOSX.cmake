204be2c9fc306f87e5c41a9e752535e2a7edb5a3
Lars Baehren <lbaehren@gmail.com>
2012-11-05 11:03:41 +0100

    Extract MacPorts list from configuration file.

--- packages/PackagesOSX.cmake
+++ packages/PackagesOSX.cmake
@@ -13,44 +13,56 @@ find_program (PORT_EXECUTABLE port
 
 if (PORT_EXECUTABLE)
 
-  message (STATUS "Checking MacPorts installation ...")
+  if (config_packages_osx_yml)
 
-  ## List of packages ______________________________________
+    message (STATUS "Checking MacPorts installation ...")
 
-  foreach (varPackage
-      doxygen
-      mod_ruby
-      mysql5-devel
-      rb-rubygems
-      ruby
+    ## Initialize counters
+    set (NOF_PACKAGES       0 )
+    set (NOF_PACKAGES_FOUND 0 )
+
+    ## Read configuration file with the list of required gems
+    file (STRINGS ${config_packages_osx_yml} _configPackages
+      REGEX "name:"
       )
 
-    ## Check if package if installed already _______________
+    foreach (varPackage ${_configPackages})
 
-    execute_process (
-      COMMAND ${PORT_EXECUTABLE} list ${varPackage}
-      WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
-      RESULT_VARIABLE PORT_RESULT_VARIABLE
-      OUTPUT_VARIABLE PORT_OUTPUT_VARIABLE
-      ERROR_VARIABLE PORT_ERROR_VARIABLE
-      OUTPUT_STRIP_TRAILING_WHITESPACE
-      )
+      ## Extract the name of the gems from the line in the configuration file
+      string (REGEX REPLACE "- name: " "" varPackage ${varPackage})
 
-    ## Installation instructions for the package ___________
+      ## Check if package if installed already _______________
 
-    if (PORT_OUTPUT_VARIABLE)
-      message ("   [OK] ${varPackage}")
-    else (PORT_OUTPUT_VARIABLE)
-      message ("   [--] ${varPackage}")
-      add_custom_command (
-	TARGET InstallPackages
-	POST_BUILD
-	COMMAND ${PORT_EXECUTABLE} install ${varPackage}
+      execute_process (
+	COMMAND ${PORT_EXECUTABLE} list ${varPackage}
 	WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
-	COMMENT "Installing MacPorts package ${varPackage} ..."
+	RESULT_VARIABLE PORT_RESULT_VARIABLE
+	OUTPUT_VARIABLE PORT_OUTPUT_VARIABLE
+	ERROR_VARIABLE PORT_ERROR_VARIABLE
+	OUTPUT_STRIP_TRAILING_WHITESPACE
 	)
-    endif (PORT_OUTPUT_VARIABLE)
 
-  endforeach (varPackage)
+      ## Installation instructions for the package ___________
+
+      if (PORT_OUTPUT_VARIABLE)
+	message ("   [OK] ${varPackage}")
+      else (PORT_OUTPUT_VARIABLE)
+	message ("   [--] ${varPackage}")
+	add_custom_command (
+	  TARGET InstallPackages
+	  POST_BUILD
+	  COMMAND ${PORT_EXECUTABLE} install ${varPackage}
+	  WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+	  COMMENT "Installing MacPorts package ${varPackage} ..."
+	  )
+      endif (PORT_OUTPUT_VARIABLE)
+
+    endforeach (varPackage)
+
+  else (config_packages_osx_yml)
+
+    message (STATUS "Missing packages_osx.yml configuration file!")
+
+  endif (config_packages_osx_yml)
 
 endif (PORT_EXECUTABLE)
