8293003cb4867419a24f053043a844a3b2a4f78a
Lars Baehren <lbaehren@gmail.com>
2012-11-16 14:21:56 +0100

    Specific information on individual gems should not reside directly within the configuration scripts, but should be stored with the configuration files.

--- packages/RubyGems.cmake
+++ packages/RubyGems.cmake
@@ -5,10 +5,6 @@ add_custom_target (InstallGems
 
 find_package (RubyGems)
 
-## Specific version
-set (FERRET_REQUIRED_VERSION  "0.11.8.1" )
-set (UNICODE_REQUIRED_VERSION "0.1.1"    )
-
 ##_______________________________________________________________________________
 ## Installation of the individual packages
 
@@ -83,34 +79,6 @@ if (GEM_EXECUTABLE)
     message (STATUS "Missing gems.yml configuration file!")
 
   endif (config_gems_yml)
-
-  ##________________________________________________________
-  ## Gems for which a specific version is required
-  ## (see 'pandora/config/environment.rb')
-  ##
-  ## 'ferret'      => '= 0.11.8.1',
-  ## 'libxml-ruby' => '>= 1.1.3',
-  ## 'unicode'     => '>= 0.1.1'
-
-
-  execute_process (
-    COMMAND ${GEM_EXECUTABLE} list -i ferret -v ${FERRET_REQUIRED_VERSION}
-    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
-    RESULT_VARIABLE GEM_RESULT_VARIABLE
-    OUTPUT_VARIABLE GEM_OUTPUT_VARIABLE
-    ERROR_VARIABLE GEM_ERROR_VARIABLE
-    OUTPUT_STRIP_TRAILING_WHITESPACE
-    )
-
-  if (${GEM_OUTPUT_VARIABLE} STREQUAL "false")
-    add_custom_command (
-      TARGET InstallGems
-      POST_BUILD
-      COMMAND ${GEM_EXECUTABLE} install ferret -v ${FERRET_REQUIRED_VERSION} --source http://prometheus-app.uni-koeln.de/rubygems
-      WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
-      COMMENT "Installing Ruby Gem ferret (${FERRET_REQUIRED_VERSION}) ..."
-    )
-  endif ()
 
   ## Summary of Gem check
 
