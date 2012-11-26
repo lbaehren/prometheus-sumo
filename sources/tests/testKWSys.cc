0131892bbd6c1af0da0cc3492bc99aef5b66edf6
Lars Baehren <lbaehren@gmail.com>
2012-11-26 15:49:13 +0100

    A few basic tests for working with external libraries.

---
+++ sources/tests/testKWSys.cc
@@ -0,0 +1,64 @@
+/***************************************************************************
+ *   Copyright (C) 2012                                                    *
+ *   Lars B"ahren (lbaehren@gmail.com)                                     *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+/*!
+  \file testKWSys.cc
+  \ingroup prometheus
+  \ingroup tests
+  \author Lars Baehren
+  \date 2012-11-26
+*/
+
+#include <iostream>
+#include <kwsys/Configure.hxx>
+#include <kwsys/SystemInformation.hxx>
+
+int test_Check ()
+{
+  std::cout << "\n[testKWSys::test_Check]\n" << std::endl;
+
+  kwsys::SystemInformation sysInfo;
+
+  sysInfo.RunCPUCheck ();
+  sysInfo.RunOSCheck();
+  sysInfo.RunMemoryCheck();
+
+  std::cout << "-- Hostname ................ = " << sysInfo.GetHostname()    << std::endl;
+  std::cout << "-- Processor clock frequency = " << sysInfo.GetProcessorClockFrequency()
+	    << std::endl;
+  std::cout << "-- OS name ................. = " << sysInfo.GetOSName ()            << "\n";
+  std::cout << "-- OS Release .............. = " << sysInfo.GetOSRelease ()         << "\n";
+  std::cout << "-- OS Version .............. = " << sysInfo.GetOSVersion ()         << "\n";
+  std::cout << "-- OS platform ............. = " << sysInfo.GetOSPlatform ()        << "\n";
+  std::cout << "-- Total memory ............ = " << sysInfo.GetHostMemoryTotal()    << "\n";
+  std::cout << "-- Total virtual memory .... = " << sysInfo.GetTotalVirtualMemory() << "\n";
+
+  return 0;
+}
+
+int main (int argc, char* argv[])
+{
+  int status = 0;
+
+  status += test_Check ();
+
+  return status;
+}
+
---
+++ sources/tests/testPoDoFo.cc
@@ -0,0 +1,52 @@
+/***************************************************************************
+ *   Copyright (C) 2012                                                    *
+ *   Lars B"ahren (lbaehren@gmail.com)                                     *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+/*!
+  \file testPoDoFo.cc
+  \ingroup prometheus
+  \ingroup tests
+  \author Lars Baehren
+  \date 2012-11-26
+ */
+
+#include <iostream>
+#include <string>
+#include <podofo/doc/PdfMemDocument.h>
+
+int main (int argc, char* argv[])
+{
+  int status = 0;
+  std::string filename;
+
+  if ( argc>1 ) {
+    filename = std::string(argv[1]);
+
+    PoDoFo::PdfMemDocument pdf (filename.c_str());
+    PoDoFo::PdfInfo * info = pdf.GetInfo();
+
+    std::cout << "-- Filename   = " << filename << std::endl;
+    std::cout << "-- Title      = " << info->GetTitle().GetStringUtf8() << std::endl;
+    std::cout << "-- Author     = " << info->GetAuthor().GetStringUtf8() << std::endl;
+    std::cout << "-- Creator    = " << info->GetCreator().GetStringUtf8() << std::endl;
+    std::cout << "-- nof. pages = " << pdf.GetPageCount() << std::endl;
+  }
+
+  return status;
+}
