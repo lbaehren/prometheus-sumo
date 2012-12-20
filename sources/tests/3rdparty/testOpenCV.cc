5d1bfbbaf2a1d4ea658452f1f9c0aee221659fba
Lars Baehren <lbaehren@gmail.com>
2012-12-20 13:10:14 +0100

    Clean up directory with test programs, collecting tests for 3rd-party packages in separate folder.

---
+++ sources/tests/3rdparty/testOpenCV.cc
@@ -0,0 +1,52 @@
+/***************************************************************************
+ *   Copyright (C) 2012                                                    *
+ *   Lars B"ahren (lbaehren@gmail.com)                                     *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+/*!
+  \file testOpenCV.cc
+  \ingroup prometheus
+  \ingroup tests
+  \author Lars Baehren
+  \date 2012-12-20
+ */
+
+#include <iostream>
+#include <opencv2/highgui/highgui_c.h>
+
+// === Program main function ===================================================
+
+//! Program main function
+int main (int argc, char *argv[])
+{
+  int status = 0;
+  std::string filename;
+
+  /* Check for command line arguments */
+  if (argc>2) {
+    filename = std::string(argv[2]);
+    //load image from directory
+    IplImage* img = cvLoadImage(filename.c_str());
+  } else {
+    std::cerr << "Now input image specified - aborting!" << std::endl;
+    status = -1;
+  }
+
+  return status;
+}
+
